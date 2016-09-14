ed321d416526a0edb62ccb2737fc8bb2f5cdddae
jacqschweiger <jacqschweiger@gmail.com>
2016-09-14 10:49:23 -0400

    Done

--- Cards/Cards/ViewController.swift
+++ Cards/Cards/ViewController.swift
@@ -10,10 +10,69 @@ import UIKit
 
 class ViewController: UIViewController {
     // TODO: Create IB outlets
+    
+    @IBOutlet weak var numberField: UILabel!
+    
+    @IBOutlet weak var suitField1: UILabel!
+   
+    @IBOutlet weak var suitField2: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+            numberField.hidden = true
+            suitField1.hidden = true
+            suitField2.hidden = true
     }
-
-    // TODO: IB actions and code to update UI
+    
+    
+    func unhide() {
+        numberField.hidden = false
+        suitField1.hidden = false
+        suitField2.hidden = false
+    }
+    
+    
+    @IBAction func clickButton4(sender: AnyObject) {
+        unhide()
+        numberField.text = "10"
+        suitField1.text = "♥️"
+        suitField2.text = "♥️"
+    }
+    
+    @IBAction func clickButton3(sender: AnyObject) {
+        unhide()
+        numberField.text = "8"
+        suitField1.text = "♦️"
+        suitField2.text = "♦️"
+    }
+    
+    @IBAction func clickButton2(sender: AnyObject) {
+        unhide()
+        numberField.text = "3"
+        suitField1.text = "♠️"
+        suitField2.text = "♠️"
+    }
+    
+    @IBAction func clickButton1(sender: AnyObject) {
+        unhide()
+        numberField.text = "4"
+        suitField1.text = "♣️"
+        suitField2.text = "♣️"
+    }
+   
+    
+    
+    
 }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
